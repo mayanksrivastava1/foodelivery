06527375036bbf3c49a94afcf50790ac7617b493
mayanksrivastava1 <100144534+mayanksrivastava1@users.noreply.github.com>
2023-06-19 09:04:20 +0530

    Update Login.js

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -23,6 +23,7 @@ export default function Login() {
     if(json.success){
       localStorage.setItem("userEmail",credentials.email);
       localStorage.setItem("authToken",json.authToken);
+      alert("Successfully Logged-In)
       navigate("/")
     }
   }
